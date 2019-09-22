54aaa0c38a7a75f499a03ba2877bdd8a15935fa2
duqing <860435288@qq.com>
2019-09-22 18:46:46 +0800

    add search

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,13 +4,11 @@ urls:
 # Author     : rexdu
 # FileName   : urls.py
 # CreateDate : 2019-09-21 22:00
-from django.conf.urls import url
-from django.urls import path, include
+from django.urls import path
 
-from .views import HomeView, CategoryView
+from .views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    url(r'category/(?P<category>\w+)', CategoryView.as_view(), name='category'),
 ]
 
--- apps/home/views.py
+++ apps/home/views.py
@@ -4,26 +4,20 @@ from pure_pagination import Paginator
 from rest_framework.views import APIView
 
 from home.models import Article
+
+
 # Create your views here.
 
 
 class HomeView(APIView):
     def get(self, request):
-        articles = Article.objects.order_by('title')
+        keyword = request.GET.get('keyword')
+        if keyword:
+            articles = Article.objects.filter(title__icontains=keyword).order_by('title')
+        else:
+            articles = Article.objects.order_by('title')
         page = request.GET.get('page', 1)
-        paginator = Paginator(articles, 11, request=request)
-        try:
-            curr_page = paginator.page(page)
-        except PageNotAnInteger:
-            curr_page = paginator.page(1)
-        return render(request, 'home.html', locals())
-
-
-class CategoryView(APIView):
-    def get(self, request, category):
-        articles = Article.objects.filter(title__icontains=category).order_by('title')
-        page = request.GET.get('page', 1)
-        paginator = Paginator(articles, 11, request=request)
+        paginator = Paginator(articles, 10, request=request)
         try:
             curr_page = paginator.page(page)
         except PageNotAnInteger:
