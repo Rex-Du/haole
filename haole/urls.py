d315a348c48c8601f3eac979471659c158bd211c
dWX568469 <duqing10@huawei.com>
2019-09-23 15:12:18 +0800

    add icon

--- haole/urls.py
+++ haole/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.home.urls')),
     path('article/', include('apps.detail.urls')),
+    url(r'^favicon.ico$', RedirectView.as_view(url=r'static/favicon.ico')),
 ]
