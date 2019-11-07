6b21a25dedaf7d5467a153a84d69a1fa234ff803
duqing <860435288@qq.com>
2019-11-07 21:55:46 +0800

    update

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,11 +1,13 @@
 from django.core.paginator import PageNotAnInteger
 from django.shortcuts import render
+from django.db.models import Q
 from pure_pagination import Paginator
 from rest_framework.views import APIView
 
 from home.models import Article
 
 from rest_framework.authentication import BasicAuthentication
+
 
 # Create your views here.
 
@@ -14,8 +16,9 @@ class HomeView(APIView):
     def get(self, request):
         keyword = request.GET.get('keyword')
         if keyword:
-            articles = Article.objects.filter(title__icontains=keyword).order_by('title').values(
-                'id', 'title', 'platform')
+            articles = Article.objects.filter(
+                Q(title__icontains=keyword) | Q(content_html__icontains=keyword)).order_by(
+                'title').values('id', 'title', 'platform')
         else:
             articles = Article.objects.order_by('title').values('id', 'title', 'platform')
         page = request.GET.get('page', 1)
