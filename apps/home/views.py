9e749b2bae7ba81495949335d9d9e2a458598bd1
rexdu <860435288@qq.com>
2020-07-02 22:48:20 +0800

    去掉es，改用mysql全文索引

--- apps/home/views.py
+++ apps/home/views.py
@@ -14,17 +14,18 @@ from home.models import Article
 
 from rest_framework.authentication import BasicAuthentication
 
-es = Elasticsearch(["111.229.61.201:9200"])
 
-
-# Create your views here.
-class ESPaginator:
-    def __init__(self, request, per_page, count):
-        self.request = request
-        self.per_page = per_page
-        self.count = count
-        hits = max(1, self.count)
-        self.num_pages = int(ceil(hits / float(self.per_page)))
+# es = Elasticsearch(["111.229.61.201:9200"])
+#
+#
+# # Create your views here.
+# class ESPaginator:
+#     def __init__(self, request, per_page, count):
+#         self.request = request
+#         self.per_page = per_page
+#         self.count = count
+#         hits = max(1, self.count)
+#         self.num_pages = int(ceil(hits / float(self.per_page)))
 
 
 class HomeView(APIView):
@@ -35,28 +36,45 @@ class HomeView(APIView):
         if keyword:
             # articles = Article.objects.filter(title__icontains=keyword).order_by('title').values(
             #     'id', 'title', 'platform')
-            body = {
-                "query": {
-                    "match_phrase": {
-                        "content_html": {
-                            "query": keyword
-                        }
-                    }
-                },
-                "_source": ["id", "title", "platform", "status"],
-                "size": per_page,
-                "from": (int(page)-1)*per_page
-            }
-            results = es.search(index='haolearticle', body=body)
-            articles = [result.get('_source') for result in results.get('hits').get('hits')]
-            curr_page = Page(articles, page, ESPaginator(request, per_page, count=results.get('hits').get('total')))
+            # body = {
+            #     "query": {
+            #         "match_phrase": {
+            #             "content_html": {
+            #                 "query": keyword
+            #             }
+            #         }
+            #     },
+            #     "_source": ["id", "title", "platform", "status"],
+            #     "size": per_page,
+            #     "from": (int(page)-1)*per_page
+            # }
+            # results = es.search(index='haolearticle', body=body)
+            # articles = [result.get('_source') for result in results.get('hits').get('hits')]
+            # curr_page = Page(articles, page, ESPaginator(request, per_page, count=results.get('hits').get('total')))
+            from django.db import connection
+            class _Article:
+                id = None
+                title = None
+                content_html = None
+
+            cursor = connection.cursor()
+            sql = f"select id, title, platform from home_article where math(`content_html`) against ('\"{keyword}\"' in boolean mode)"
+            cursor.execute()
+            results = cursor.fetchall()
+            articles = list()
+            for result in results:
+                article = _Article()
+                article.id = request[0]
+                article.title = result[1]
+                article.content_html = result[2]
+                articles.append(article)
         else:
             articles = Article.objects.filter(status=1).order_by('title').values('id', 'title', 'platform')
-            paginator = Paginator(articles, per_page, request=request)
-            try:
-                curr_page = paginator.page(page)
-            except PageNotAnInteger:
-                curr_page = paginator.page(1)
+        paginator = Paginator(articles, per_page, request=request)
+        try:
+            curr_page = paginator.page(page)
+        except PageNotAnInteger:
+            curr_page = paginator.page(1)
         return render(request, 'home.html', locals())
 
 
@@ -64,8 +82,8 @@ class DeleteView(APIView):
     @csrf_exempt
     def post(self, request):
         id = request.POST.get('id')
-        query = {'query': {'match': {'id': id}}}
-        es.delete_by_query(index='haolearticle', body=query)
+        # query = {'query': {'match': {'id': id}}}
+        # es.delete_by_query(index='haolearticle', body=query)
         article = Article.objects.get(id=id)
         article.status = 0
         article.save()
